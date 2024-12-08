af7ca050d49a2ba6b5126b6865f4b4c1873a0b86
anhtv2 <107092614+anhtv2@users.noreply.github.com>
2024-12-08 10:44:46 +0000

    Update

--- src/scripts/addNewUser.ts
+++ src/scripts/addNewUser.ts
@@ -3,11 +3,11 @@ import bcrypt from "bcrypt";
 import { SALT_ROUND } from "../constants/saltRound";
 
 (async () => {
-  const username = "pdhung";
-  const passwordRaw = "hungdeptrai";
-  const email = `pdhung@gmail.com`;
-  const phone = "0959696996";
-  const name = "Pham Duy Hung";
+  const username = "nhom3";
+  const passwordRaw = "kstn2024@";
+  const email = `iamkstn@gmail.com`;
+  const phone = "0987654321";
+  const name = "Nhom 3";
   const topic = "abc";
   const password = bcrypt.hashSync(passwordRaw, SALT_ROUND);
   return addUser({
